d8eedfd6512aaee0bc7881b4febaf4748c543116
miguelag99 <miguelag99@hotmail.com>
2026-04-20 15:08:37 +0200

    fix: add FocalLoss to losses __init__

--- tgrip/loss/__init__.py
+++ tgrip/loss/__init__.py
@@ -1,3 +1,3 @@
 from .common import LossCollection, LossInterface, Weighting, unpack_nested_dict
-from .segmentation import BCELoss, SpatialLoss, CELoss
+from .segmentation import BCELoss, SpatialLoss, CELoss, FocalLoss
 from .cosine_similarity import CosineSimilarityLoss
